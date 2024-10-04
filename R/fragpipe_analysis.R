1b882d5dac570ad3ef76919a63caceb57cb95d7c
jflucier <3505568+jflucier@users.noreply.github.com>
2024-10-04 14:00:33 -0400

    group comparison patch

--- R/fragpipe_analysis.R
+++ R/fragpipe_analysis.R
@@ -11,12 +11,14 @@
 # renv::install("bioc::cmapR", prompt=FALSE)
 # renv::install("bioc::ConsensusClusterPlus", prompt=FALSE)
 # renv::install("Nesvilab/FragPipeAnalystR", prompt=FALSE)
+renv::install("bioc::SEtools", prompt=FALSE)
 # 
 # # optional
 # renv::install("nicolerg/ssGSEA2", prompt=FALSE)
 
 library(FragPipeAnalystR)
 library("optparse")
+library(SEtools)
 
 ### first filter report.pg_matrix.tsv to select proteotypic proteins groups ###
 # perl -ne '
@@ -28,10 +30,10 @@ library("optparse")
 # }
 # ' report.pg_matrix.tsv > report.pg_matrix.proteoptypic.tsv
 
-# bp <- "/storage/Documents/service/externe/sheela/20240729_mouse_ms_lysM"
+# bp <- "/storage/Documents/service/externe/sheela/20240829_HSCs_liver_mouse"
 # out_bp <- paste0(bp,"/fragpipe")
-# in_diann <- "/storage/Documents/service/externe/sheela/20240729_mouse_ms_lysM/results/report.pg_matrix.proteoptypic.tsv"
-# in_design <- "/storage/Documents/service/externe/sheela/20240729_mouse_ms_lysM/experiment_annotation.tsv"
+# in_diann <- "/storage/Documents/service/externe/sheela/20240829_HSCs_liver_mouse/results/report.pg_matrix.proteoptypic.tsv"
+# in_design <- "/storage/Documents/service/externe/sheela/20240829_HSCs_liver_mouse/experiment_annotation.outliers.tsv"
 
 option_list = list(
   make_option(c("-o", "--out"), type="character", default=NULL, help="output dir", metavar="character"),
@@ -69,27 +71,54 @@ plot_missval_heatmap(ccrcc)
 plot_feature_numbers(ccrcc)
 dev.off()
 
-# manual_impute imputes missing values in a proteomics dataset by random draws from a manually defined distribution.
-pdf(
-  paste0(out_bp,"/report.pg_matrix.proteoptypic.qc_imputed.pdf")
-)
-imputed <- manual_impute(ccrcc)
-plot_pca(
-  imputed
-)
-
-plot_correlation_heatmap(imputed)
-plot_feature_numbers(imputed)
-dev.off()
+# # manual_impute imputes missing values in a proteomics dataset by random draws from a manually defined distribution.
+# pdf(
+#   paste0(out_bp,"/report.pg_matrix.proteoptypic.qc_imputed.pdf")
+# )
+# imputed <- manual_impute(ccrcc)
+# plot_pca(
+#   imputed
+# )
+# 
+# plot_correlation_heatmap(imputed)
+# plot_feature_numbers(imputed)
+# dev.off()
 
 pdf(
   paste0(out_bp,"/report.pg_matrix.proteoptypic.dge.pdf")
 )
-de_result <- test_limma(ccrcc, type = "all")
+
+group_list <- unique(ccrcc@colData@listData[["condition"]])
+ctrl <- grep(
+  "Ctrl", 
+  group_list, 
+  ignore.case=TRUE,
+  value=TRUE, 
+  fixed=FALSE
+)
+exp=grep(
+  "Ctrl", 
+  group_list, 
+  ignore.case=TRUE,
+  value=TRUE, 
+  fixed=FALSE,
+  invert=TRUE
+)
+
+i<-0
+for (c in group_list) {
+  de_result_tmp <- test_limma(ccrcc, type = "control", control=c)
+  if(i == 0){
+    de_result <- de_result_tmp
+  } else {
+    de_result <- mergeSEs( list(se1=de_result, se2=de_result_tmp) )
+  }
+  i<-i+1
+}
+
 # add_rejections marks significant proteins based on defined cutoffs. Deafult is <= 0.05 adjusted P value and log2 fold change >= 1
 de_result_updated <- add_rejections(de_result)
 
-group_list <- unique(de_result@colData@listData[["condition"]])
 for (value1 in group_list) {
   #print(value1)
   for (value2 in group_list) {
@@ -113,32 +142,32 @@ write.table(
   row.names = FALSE
 )
 
-pdf(
-  paste0(out_bp,"/report.pg_matrix.proteoptypic.dge_imputed.pdf")
-)
-de_result <- test_limma(imputed, type = "all")
-# add_rejections marks significant proteins based on defined cutoffs. Deafult is <= 0.05 adjusted P value and log2 fold change >= 1
-de_result_updated <- add_rejections(de_result)
-group_list <- unique(de_result@colData@listData[["condition"]])
-for (value1 in group_list) {
-  #print(value1)
-  for (value2 in group_list) {
-    v_diff <- paste0(value1,"_vs_",value2,"_diff")
-    if(!is.null(de_result_updated@elementMetadata@listData[[v_diff]])){
-      lbl <- paste0(value1,"_vs_",value2)
-      print(paste0("plotting volcano for: ", lbl))
-      p <- plot_volcano(de_result_updated, lbl, name_col = "Genes")
-      print(p)
-    }
-  }
-}
-dev.off()
-
-x <- de_result_updated@elementMetadata@listData
-y <- data.frame("rowid"=x[["Protein.Group"]],x)
-write.table(
-  data.frame("rowid"=rownames(y),x),
-  file = paste0(out_bp,"/report.pg_matrix.proteoptypic.dge_imputed.tsv"),
-  sep = "\t",
-  row.names = FALSE
-)
+# pdf(
+#   paste0(out_bp,"/report.pg_matrix.proteoptypic.dge_imputed.pdf")
+# )
+# de_result <- test_limma(imputed, type = "all")
+# # add_rejections marks significant proteins based on defined cutoffs. Deafult is <= 0.05 adjusted P value and log2 fold change >= 1
+# de_result_updated <- add_rejections(de_result)
+# group_list <- unique(de_result@colData@listData[["condition"]])
+# for (value1 in group_list) {
+#   #print(value1)
+#   for (value2 in group_list) {
+#     v_diff <- paste0(value1,"_vs_",value2,"_diff")
+#     if(!is.null(de_result_updated@elementMetadata@listData[[v_diff]])){
+#       lbl <- paste0(value1,"_vs_",value2)
+#       print(paste0("plotting volcano for: ", lbl))
+#       p <- plot_volcano(de_result_updated, lbl, name_col = "Genes")
+#       print(p)
+#     }
+#   }
+# }
+# dev.off()
+# 
+# x <- de_result_updated@elementMetadata@listData
+# y <- data.frame("rowid"=x[["Protein.Group"]],x)
+# write.table(
+#   data.frame("rowid"=rownames(y),x),
+#   file = paste0(out_bp,"/report.pg_matrix.proteoptypic.dge_imputed.tsv"),
+#   sep = "\t",
+#   row.names = FALSE
+# )
